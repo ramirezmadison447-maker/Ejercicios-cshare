7fc68c00622bb98d221386204d4e55c0215fc267
Madison Ramirez <ramirezmadison447@gmail.com>
2026-09-08 01:22:07 -0400

    Ejercicio 1 y 2 hechos en la carpeta Ejercicios

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// EJERCICIO 1: Escribe un programa en C# que imprima "Hello, World!" en la consola.
+//Console.WriteLine("Hello, World!");
+
+
+// Ejercicio 2
+
+namespace Cookinglasagna 
+{ 
+    public class Lasagna
+    {
+        static void Main(string[] args)
+        {
+            
+       
+        var lasagna = new Lasagna();
+        Console.WriteLine(lasagna.ExpectedMinutesInOven());
+        Console.WriteLine(lasagna.ElapsedTimeInMinutes(3,20));
+
+        }
+        
+        public int ExpectedMinutesInOven()
+        {
+            return 40;
+        }
+
+        public int RemainingMinutesInOven(int actualMinutesInOven)
+        {
+            return ExpectedMinutesInOven() - actualMinutesInOven;
+        }
+
+        public int PreparationTimeInMinutes(int numberOfLayers)
+        {
+            return numberOfLayers * 2;
+        }
+
+        public int ElapsedTimeInMinutes(int numberOfLayers, int actualMinutesInOven)
+        {
+            return (numberOfLayers * 2) + actualMinutesInOven;
+        }
+
+
+
+    }
+
+
+}
