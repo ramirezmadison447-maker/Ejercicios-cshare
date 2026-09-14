4b999a0c9625e209619114342be122fd68930089
Madison Ramirez <ramirezmadison447@gmail.com>
2026-09-13 23:00:14 -0400

    Ejercicio 3 - Completo

--- Ejercicio03/Program.cs
+++ Ejercicio03/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class QuestLogic
+{
+    public static void Main(string[] args)
+    {
+        var knightIsAwake = false;
+        var archerIsAwake = true;
+        var prisonerIsAwake = false;
+        var petDogIsPresent = false;
+
+        var resultado = QuestLogic.CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent);
+        Console.WriteLine(resultado);
+
+    }
+
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+    return !knightIsAwake;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+       return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        return (petDogIsPresent && !archerIsAwake) || (!knightIsAwake && !archerIsAwake && prisonerIsAwake) ;
+    }
+}
