f2f7c7e45194f2bdbd4a08cdc3b92bde7b0a1dc0
Madison Ramirez <ramirezmadison447@gmail.com>
2026-09-15 00:36:32 -0400

    Ejercicio 6 - Completado

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Linq;
+
+public class BirdCount
+{
+    private readonly int[] _birdsPerDay;
+
+    public BirdCount(int[] birdsPerDay)
+    {
+        _birdsPerDay = birdsPerDay;
+    }
+
+    public static int[] LastWeek()
+    {
+        return new[] { 0, 2, 5, 3, 7, 8, 4 };
+    }
+
+    public int Today()
+    {
+        return _birdsPerDay[^1];
+    }
+
+    public void IncrementTodaysCount()
+    {
+        _birdsPerDay[^1]++;
+    }
+
+    public bool HasDayWithoutBirds()
+    {
+        return _birdsPerDay.Contains(0);
+    }
+
+    public int CountForFirstDays(int numberOfDays)
+    {
+        return _birdsPerDay.Take(numberOfDays).Sum();
+    }
+
+    public int BusyDays()
+    {
+        return _birdsPerDay.Count(count => count >= 5);
+    }
+}
