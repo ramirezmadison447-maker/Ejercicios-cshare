10a33825a11942b9c1c2bf3a6cd335cdfcd7d437
Madison Ramirez <ramirezmadison447@gmail.com>
2026-09-19 10:08:08 -0400

    Ejercicio 08 - Completado

--- Ejercicio07/Program.cs
+++ Ejercicio07/Program.cs
@@ -8,7 +8,7 @@ public class Identifier
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine(Identifier.Clean("My   Id"));
-        Console.WriteLine(Identifier.Clean("my\0Id"));
+        Console.WriteLine(Identifier.Clean("my\nId"));
         Console.WriteLine(Identifier.Clean("à-ḃç"));
         Console.WriteLine(Identifier.Clean("1😀2😀3😀"));
         Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
---
+++ Ejercicio08/Program.cs
@@ -0,0 +1,64 @@
+﻿public class RemoteControlCar
+{
+    private int _battery = 100;
+    private int _distance = 0;
+    
+    public static RemoteControlCar Buy()
+    {
+        return new RemoteControlCar();
+    }
+
+    public string DistanceDisplay()
+    {
+        return $"Driven {_distance} meters";
+    }
+
+    public string BatteryDisplay()
+    {
+        if (_battery == 0)
+        {
+            return "Battery empty";
+
+        }
+        else
+        {
+            return $"Battery at {_battery}%";
+
+        }
+    }
+
+    public void Drive()
+    {
+        if (_battery > 0)
+        {
+            _distance += 20;
+            _battery -= 1;
+        }
+
+    }
+
+}
+
+
+
+
+class Ejecutable
+{
+    public static void Main(string[] args)
+    {
+
+        var car = RemoteControlCar.Buy();
+        
+        for (int i = 0; i < 100; i++)
+        {
+            car.Drive();
+            car.Drive();
+            car.Drive();
+            car.Drive();
+            car.Drive();
+        }
+        Console.WriteLine(car.DistanceDisplay());
+        Console.WriteLine(car.BatteryDisplay());
+
+    }
+}
